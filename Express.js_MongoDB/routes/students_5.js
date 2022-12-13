63f5fceca804ca328fb707c1576f0ee092f34223
unknown <hellomstars@naver.com>
2022-12-13 23:37:23 +0900

    index_04 특정 조건이 포함된 GET 요청을 처리하는 API 만들기 예시 활용

---
+++ Express.js_MongoDB/routes/students_5.js
@@ -0,0 +1,17 @@
+const { Router } = require("express");
+const { Student } = require("../models");
+
+const router = Router();
+
+router.get("/", async (req, res) => {
+  const search_major = req.query.major;
+
+
+  const student = await Student.find({ major: search_major });
+  
+  res.json(student);
+});
+
+
+
+module.exports = router;
