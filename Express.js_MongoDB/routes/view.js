07ea7d42ffa170a5129c5a0b4e505fc37f2b1df8
unknown <hellomstars@naver.com>
2022-12-13 23:28:49 +0900

    index_04 POST~DELETE 요청을 처리하는 API 만들기 예시 활용

--- Express.js_MongoDB/routes/view.js
+++ Express.js_MongoDB/routes/view.js
@@ -1,9 +1,7 @@
 const { Router } = require("express");
 const { Student } = require("../models");
-
 const router = Router();
 
-// 추가한 학생을 확인하기 위한 router입니다.
 router.get("/", async (req, res) => {
   const students = await Student.find({});
 
