0e097abe178f5bdeebea7619924f1365c85e3ac7
unknown <hellomstars@naver.com>
2022-12-13 23:20:46 +0900

    index_04 DELETE 요청을 처리하는 API 만들기 예시 활용

---
+++ Express.js_MongoDB/models/schemas/types/short-id_2.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const { Student } = require("../models");
+
+const router = Router();
+
+router.delete("/:shortId", async (req, res) => {
+  const { shortId } = req.params;
+
+  await Student.deleteOne({ shortId });
+  res.status(201).send();
+});
+
+
+module.exports = router;
