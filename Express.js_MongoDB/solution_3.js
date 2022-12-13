b5efdf9075f1d18142f15e80199d9369a1b3ecd0
unknown <hellomstars@naver.com>
2022-12-13 23:10:22 +0900

    index_04 POST 요청을 처리하는 API 만들기 예시 활용

---
+++ Express.js_MongoDB/solution_3.js
@@ -0,0 +1,23 @@
+const { Router } = require("express");
+const { Student } = require("../models");
+
+const router = Router();
+
+router.post("/", async (req, res) => {
+  const { name, age, major } = req.body;
+  
+  const student = await Student.create({
+    name,
+    age,
+    major,
+  });
+  
+  res.json(student);
+});
+
+// 학생 데이터를 등록하는 페이지를 render합니다.
+router.get("/", (req, res) => {
+  res.render("register");
+});
+
+module.exports = router;
